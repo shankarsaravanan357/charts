28180bb4d45f8c567556f14c697f75c44a5989a6
shankar <shankarsaravanan007@gmail.com>
2021-07-14 17:35:15 +0530

    sending data from javascipt to flask through app route succesfull.

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+from re import X
 from flask import Flask,render_template,url_for,request,redirect, make_response
 import random
 import json
 from time import time
 from random import random
+from flask.wrappers import Response
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, make_response
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///randomvalues.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'abcdefgh'
+
+db = SQLAlchemy(app)
+
+class Randomvalues(db.Model):
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    ranval = db.Column(db.Float)
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -14,7 +28,11 @@ def main():
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    data = [time() * 1000, random() * 100]
+    args = request.args.get('name')
+    print (args) # For debugging
+    print("helo")
+    value1 = Randomvalues.query.filter_by(id = args).first()
+    data = [time() * 1000, value1.ranval]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
